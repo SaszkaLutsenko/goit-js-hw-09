0b08653c5958ae5ae342967135edd290e090fa7a
SaszkaLutsenko <113133640+SaszkaLutsenko@users.noreply.github.com>
2024-01-08 10:00:40 +0100

    q

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,16 +1,45 @@
-const feedbackForm = document.querySelector(".feedback-form");
-feedbackForm.addEventListener("input", function (event) {
-  if (event.target.tagName === 'INPUT') {
-    const { email, message } = feedbackForm.elements;
-    const feedbackState = { email: email.value, message: message.value };
-    localStorage.setItem('feedback-form-state', JSON.stringify(feedbackState));
-  }
-});
-document.addEventListener('DOMContentLoaded', function () {
-  const savedState = localStorage.getItem('feedback-form-state');
-  if (savedState) {
-    const parsedState = JSON.parse(savedState);
-    feedbackForm.elements.email.value = parsedState.email || '';
-    feedbackForm.elements.message.value = parsedState.message || '';
-  }
-});
+const form = document.querySelector(".feedback-form");
+const localStorageKey = "feedback-data";
+
+const formObject = {};
+const getLocalStorageData = localStorage.getItem(localStorageKey);
+
+if (getLocalStorageData !== null && getLocalStorageData !== undefined) {
+    try {
+       const localStorageData = JSON.parse(getLocalStorageData);
+
+        if (localStorageData) {
+        form.elements.email.value = localStorageData.email;
+        form.elements.message.value = localStorageData.message;
+            
+        formObject.email = localStorageData.email;
+        formObject.message = localStorageData.message;
+    }
+    } catch (e) {}  
+}
+
+form.addEventListener("input", () => {
+    const formData = new FormData(form);
+    
+    formData.forEach((value, key) => {
+        formObject[key] = value.trim();
+    })
+
+    localStorage.setItem(localStorageKey, JSON.stringify(formObject))
+})
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const email = e.currentTarget.elements.email.value;
+    const message = e.currentTarget.elements.message.value;
+
+    if (email === "" || message === "") {
+        alert('All form fields must be filled in');
+    } else {
+        console.log(formObject);
+        form.reset()
+       
+    } 
+})
